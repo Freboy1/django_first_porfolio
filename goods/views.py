a0d073fba5c6dd0af9f8970ffc606cbd246b5a17
Freboy1 <dautovalisher33@gmail.com>
2024-03-24 19:50:15 +0600

    second pagination get parameters

--- goods/views.py
+++ goods/views.py
@@ -5,7 +5,9 @@ from goods.models import Products
 
 
 # Create your views here.
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    page = request.GET.get('page', 1)
+
     if category_slug=='all':
         goods = Products.objects.all()
     else:
@@ -13,7 +15,7 @@ def catalog(request, category_slug, page=1):
 
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         "title": "Home - Каталог",
